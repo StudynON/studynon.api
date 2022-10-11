a58b404fe67e2a9c8cafbf8d87993d28661092aa
Dan <dansenpirdev@gmail.com>
2022-10-11 20:43:10 -0300

    chore: runs initial migrations

---
+++ prisma/migrations/20221011233414_initial_migration/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "student" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profile_picture" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "material" (
+    "id" SERIAL NOT NULL,
+    "media" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "lifetime_access" BOOLEAN NOT NULL,
+    "deadline" TIMESTAMP(3),
+    "school" TEXT NOT NULL,
+    "id_student" TEXT NOT NULL,
+    "id_category" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "material_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "student_email_key" ON "student"("email");
+
+-- AddForeignKey
+ALTER TABLE "material" ADD CONSTRAINT "material_id_student_fkey" FOREIGN KEY ("id_student") REFERENCES "student"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "material" ADD CONSTRAINT "material_id_category_fkey" FOREIGN KEY ("id_category") REFERENCES "category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
