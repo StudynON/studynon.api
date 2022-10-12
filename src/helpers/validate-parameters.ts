57b5a996386ca6d0a73c6e53c5d90a72ea316be8
Dan <dansenpirdev@gmail.com>
2022-10-12 14:17:21 -0300

    feat: creates isString validate method

--- src/helpers/validate-parameters.ts
+++ src/helpers/validate-parameters.ts
@@ -2,4 +2,8 @@ export default class Validate {
   public static isNotEmpty = (param: string | number): boolean => {
     return !(param == null || param == undefined || param === '');
   };
+
+  public static isString = (param: string): boolean => {
+    return typeof param == 'string' && typeof param != 'number';
+  };
 }
