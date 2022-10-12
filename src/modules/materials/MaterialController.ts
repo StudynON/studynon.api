9efbfebb1abaa716258481933a271fddd3e04f2e
Dan <dansenpirdev@gmail.com>
2022-10-12 17:18:53 -0300

    feat: creates update MaterialController method

--- src/modules/materials/MaterialController.ts
+++ src/modules/materials/MaterialController.ts
@@ -2,25 +2,38 @@ import { Request, Response } from 'express';
 import { MaterialRepository } from './repositories/MaterialRepository';
 import CreateMaterial from './useCase/CreateMaterial';
 import FindOneMaterial from './useCase/FindOneMaterial';
+import UpdateMaterial from './useCase/UpdateMaterial';
 
 const repository = new MaterialRepository();
 
 export default class MaterialController {
   static async create(req: Request, res: Response) {
     const { material } = req.body;
+
     const createMaterial = new CreateMaterial(repository);
 
     await createMaterial.execute(material);
 
-    return res.send(201);
+    return res.sendStatus(201);
   }
 
   static async findOneById(req: Request, res: Response) {
     const { id } = req.body;
+
     const findOneMaterial = new FindOneMaterial(repository);
 
     const material = await findOneMaterial.execute(id);
 
     return res.status(200).json(material);
+  }
+
+  static async update(req: Request, res: Response) {
+    const { material } = req.body;
+
+    const updateMaterial = new UpdateMaterial(repository);
+
+    await updateMaterial.execute(material);
+
+    return res.sendStatus(201);
   }
 }
