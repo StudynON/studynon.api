503ca94c325f50c780e1abc8d41c88b3f509a9a5
Dan <dansenpirdev@gmail.com>
2022-10-12 12:11:47 -0300

    feat: creates IMaterial interface

---
+++ src/interfaces/IMaterial.ts
@@ -0,0 +1,10 @@
+export interface IMaterial {
+  media: string;
+  link: string;
+  title: string;
+  lifetime_access: boolean;
+  deadline: Date | null;
+  school: string;
+  id_student: string;
+  id_category: number;
+}
