d513e680bd683d8399f15b9d56f13ce5d390bf3d
Eric Oliveira <oiericoi@hotmail.com>
2022-10-13 09:56:35 -0300

    feat: add update student use case

---
+++ src/modules/student/useCase/UpdateStudent.ts
@@ -0,0 +1,30 @@
+import { hash } from 'bcrypt';
+import { IStudentUpdate } from '../../../interfaces/IStudent';
+import { StudentRepository } from '../reporitories/StudentRepository';
+
+export class CreateStudent {
+  private repository;
+
+  constructor(repository: StudentRepository) {
+    this.repository = repository;
+  }
+
+  public async execute({
+    id,
+    name,
+    email,
+    profile_picture,
+    password
+  }: IStudentUpdate): Promise<void> {
+    const hashedPassword = await hash(password, 14);
+
+    await this.repository.update(
+      id,
+    {
+      name,
+      email,
+      profile_picture,
+      password: hashedPassword,
+    } as IStudentUpdate);
+  }
+}
