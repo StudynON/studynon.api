523033534563229fcf277af4332550390f6267e1
Guilherme Siqueira <siqueiraguilherme516@gmail.com>
2022-10-14 15:01:13 -0300

    feat: add student route in index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { materialRouter } from './material.route';
 import { rootRouter } from './root.routes';
+import { studentRouter } from './student.route';
 
 export const routes = Router();
 
+routes.use('/student', studentRouter);
 routes.use('/material', materialRouter);
 routes.use('/', rootRouter);
