8d9f170023e8bc7c609feb2b60d4f0982c80d69b
Guilherme Siqueira <siqueiraguilherme516@gmail.com>
2022-10-14 14:58:56 -0300

    feat: add update and delete student in controller

--- src/modules/student/StudentController.ts
+++ src/modules/student/StudentController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { StudentRepository } from './repositories/StudentRepository';
 import { CreateStudent } from './useCase/CreateStudent';
+import { DeleteStudent } from './useCase/DeleteStudent';
+import { UpdateStudent } from './useCase/UpdateStudent';
 
 const repository = new StudentRepository();
 
@@ -14,9 +16,30 @@ export default class StudentController {
       name,
       email,
       profile_picture,
-      password
+      password,
     });
 
     return res.sendStatus(201);
+  }
+
+  static async update(req: Request, res: Response) {
+    const id = req.params.id;
+
+    const { name, email, profile_picture, password } = req.body;
+    const updateStudent = new UpdateStudent(repository);
+
+    await updateStudent.execute({ id, name, email, profile_picture, password });
+
+    return res.sendStatus(200);
+  }
+
+  static async delete(req: Request, res: Response) {
+    const id = req.params.id;
+
+    const deleteStudent = new DeleteStudent(repository);
+
+    await deleteStudent.execute(id);
+
+    return res.sendStatus(200);
   }
 }
