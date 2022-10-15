fdb9fb9854b92a02bf13031bf0b5ed4b520b3592
Eric Oliveira <oiericoi@hotmail.com>
2022-10-15 13:49:45 -0300

    feat: implement middleware in student routes

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express';
+import { validateStudent, validateStudentId } from '../middlewares/student';
 import StudentController from '../modules/student/StudentController';
 
 export const studentRouter = Router();
 
-studentRouter.post('/', StudentController.create);
-studentRouter.put('/update/:id', StudentController.update);
-studentRouter.delete('/delete/:id', StudentController.delete);
+studentRouter.post('/',
+  validateStudent,
+  StudentController.create
+);
+
+studentRouter.put('/update/:id',
+  validateStudentId,
+  validateStudent,
+  StudentController.update
+);
+
+studentRouter.delete('/delete/:id',
+  validateStudentId,
+  StudentController.delete
+);
