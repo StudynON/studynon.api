1f25f9f576ffc0691c36e1ca93503eaf318d4ab4
Victor Z <strkergames@gmail.com>
2022-10-18 21:39:22 -0300

    feat: creates login route
    Co-authored-by: viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>

---
+++ src/modules/student/useCase/LoginStudent.ts
@@ -0,0 +1,51 @@
+import { StudentRepository } from '../repositories/StudentRepository';
+import { sign } from 'jsonwebtoken';
+import { jwt_secret } from '../../../config/vars';
+import { HttpException } from '../../../errors/HttpException';
+import Validate from '../../../helpers/validate-parameters';
+import hash from 'bcrypt';
+
+interface ILoginProps {
+  email: string;
+  password: string;
+}
+
+export class LoginStudant {
+  private studantRepository;
+
+  constructor (studantRepository: StudentRepository) {
+    this.studantRepository = studantRepository;
+  }
+
+
+  public async execute ({email, password}: ILoginProps) {
+    const invalidData = [];
+
+    if (!Validate.isEmail(email)) {
+      invalidData.push('Invalid email.');
+    }
+
+    if (!Validate.isString(password) || !Validate.isNotEmpty(password)) {
+      invalidData.push('Invalid password.');
+    }
+
+    if (invalidData.length) {
+      throw new HttpException(400, `Ivalid fields: ${invalidData.join(', ')}`);
+    }
+
+    const student = await this.studantRepository.findByEmail(email);
+
+    if (!student || !hash.compareSync(password, student.password)) {
+      throw new HttpException(400, 'Invalid credentials.');
+    }
+
+    const payload = {
+      id: student.id,
+      exp: Math.floor(Date.now() / 1000) + 3600,
+    };
+
+    const token = sign(payload, jwt_secret);
+
+    return token;
+  }
+}
