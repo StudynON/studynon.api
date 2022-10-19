d1d59bc4ee5ab28e0c933123dd56a85de8dc9f52
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-10-19 09:17:52 -0300

    refactor: removes eslint bypass

--- src/middlewares/error/index.ts
+++ src/middlewares/error/index.ts
@@ -5,14 +5,12 @@ export const ErrorException = (
   err: HttpException,
   req: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
+
   if (err instanceof HttpException) {
     return res.status(err.status).send({ message: err.message });
   }
 
-  console.error(err);
-
-  return res.sendStatus(500);
+  return next(res.sendStatus(500));
 };
