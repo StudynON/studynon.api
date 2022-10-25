313f124a9d219fcb3e54bfb6137d33f11fbba0b2
Eric Oliveira <oiericoi@hotmail.com>
2022-10-25 16:55:58 -0300

    feat: add category repository

---
+++ src/modules/category/repository/CategoryRepository.ts
@@ -0,0 +1,69 @@
+import { prisma as db } from '../../../lib/prisma';
+import { ICategory } from '../../../interfaces/ICategory';
+
+export class CategoryRepository {
+  async create(category: ICategory) {
+    await db.category.create({
+      data: category
+    });
+  }
+
+  async findOneById(id: number, studentId: string) {
+    const category = await db.category.findFirst({
+      where: {
+        AND: [
+          {
+            id_student: {
+              equals: studentId
+            }
+          },
+          {
+            id: {
+              equals: id
+            }
+          }
+        ]
+      },
+    });
+
+    return category;
+  }
+
+  async findAll(studentId: string) {
+    const categorys = await db.category.findMany({
+      where: {
+        id_student: studentId
+      }
+    });
+
+    return categorys;
+  }
+
+  async update(id: number, data: ICategory) {
+    await db.category.update({
+      where: {
+        id: id
+      },
+      data: data
+    });
+  }
+
+  async delete(id: number, studentId: string) {
+    await db.category.deleteMany({
+      where: {
+        AND: [
+          {
+            id: {
+              equals: id
+            }
+          },
+          {
+            id_student: {
+              equals: studentId
+            }
+          }
+        ]
+      }
+    });
+  }
+}
