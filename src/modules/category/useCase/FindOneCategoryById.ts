034cfbd85a12c915ca950de20c50ac3e6c0fe8db
Eric Oliveira <oiericoi@hotmail.com>
2022-10-26 11:57:09 -0300

    refactor: modify find one category method in category use case

--- src/modules/category/useCase/FindOneCategoryById.ts
+++ src/modules/category/useCase/FindOneCategoryById.ts
@@ -1,3 +1,4 @@
+import { HttpException } from '../../../errors/HttpException';
 import { CategoryRepository } from '../repository/CategoryRepository';
 
 export class FindOneCategoryById {
@@ -7,8 +8,12 @@ export class FindOneCategoryById {
     this.repository = repository;
   }
 
-  async execute(id: number, studentId: string): Promise<object | null> {
+  async execute(id: number, studentId: string): Promise<object> {
     const category = await this.repository.findOneById(id, studentId);
+
+    if (!category) {
+      throw new HttpException(400, 'category not found');
+    }
 
     return category;
   }
