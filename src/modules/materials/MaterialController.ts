a80212720cdd76eef43fa4ec98369082f40efce4
Eric Oliveira <oiericoi@hotmail.com>
2022-11-25 16:12:58 -0300

    refactor: modify material controller

--- src/modules/materials/MaterialController.ts
+++ src/modules/materials/MaterialController.ts
@@ -10,17 +10,35 @@ const repository = new MaterialRepository();
 
 export default class MaterialController {
   static async create(req: Request, res: Response) {
-    const { material } = req.body;
+    const id_student = req.student?.id as string;
+    const {
+      media,
+      link,
+      title,
+      deadline,
+      lifetime_access,
+      school,
+      id_category,
+    } = req.body;
 
     const createMaterial = new CreateMaterial(repository);
 
-    await createMaterial.execute(material);
+    await createMaterial.execute({
+      media,
+      link,
+      title,
+      deadline,
+      lifetime_access,
+      school,
+      id_category,
+      id_student
+    });
 
     return res.sendStatus(201);
   }
 
   static async findOneById(req: Request, res: Response) {
-    const id = parseInt(req.params.id);
+    const { id } = req.params;
 
     const findOneMaterial = new FindOneMaterial(repository);
 
@@ -30,31 +48,50 @@ export default class MaterialController {
   }
 
   static async findAll(req: Request, res: Response) {
+    const id_student = req.student?.id as string;
     const findAllMaterial = new FindAllMaterial(repository);
 
-    const material = await findAllMaterial.execute();
+    const material = await findAllMaterial.execute(id_student);
 
     return res.status(200).json(material);
   }
 
   static async update(req: Request, res: Response) {
-    const id = parseInt(req.params.id);
-    const { material } = req.body;
+    const { id } = req.params;
+    const id_student = req.student?.id as string;
+    const {
+      media,
+      link,
+      title,
+      deadline,
+      lifetime_access,
+      school,
+      id_category,
+    } = req.body;
 
     const updateMaterial = new UpdateMaterial(repository);
 
-    await updateMaterial.execute(id, material);
+    await updateMaterial.execute(id, {
+      media,
+      link,
+      title,
+      deadline,
+      lifetime_access,
+      school,
+      id_category,
+      id_student,
+    });
 
-    return res.sendStatus(201);
+    return res.sendStatus(200);
   }
 
   static async delete(req: Request, res: Response) {
-    const id = parseInt(req.params.id);
+    const { id } = req.params;
 
     const deleteMaterial = new DeleteMaterial(repository);
 
     await deleteMaterial.execute(id);
 
-    return res.status(200).json();
+    return res.sendStatus(200);
   }
 }
